f5fa849e8521e2bb290448c2d4400f1268f8c89f
kunle97 <69395755+kunle97@users.noreply.github.com>
2023-08-21 11:12:01 -0400

    Update settings.py

--- keyflow_backend/settings.py
+++ keyflow_backend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -139,3 +142,8 @@ CORS_ORIGIN_WHITELIST = [
 ]
 
 STRIPE_API_KEY="sk_test_51LkoD8EDNRYu93CIBSaakI9e31tBUi23aObcNPMUdVQH2UvzaYl6uVIbTUGbSJzjUOoReHsRU8AusmDRzW7V87wi00hHSSqjhl"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path..join(BASE_DIR, 'static'),)
+
+django_heroku.settings(locals())
