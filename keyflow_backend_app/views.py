1c2bf5efa4992484dc68e12a319247fc290c2c76
kunle97 <69395755+kunle97@users.noreply.github.com>
2023-09-21 09:38:32 -0400

    Update PlaidLinkTokenView in views.py
    Initialized plaid client

--- keyflow_backend_app/views.py
+++ keyflow_backend_app/views.py
@@ -1081,7 +1081,17 @@ class StripeViewSet(viewsets.ModelViewSet):
 class PlaidLinkTokenView(APIView):
     def post(self, request):
         # Initialize the Plaid API client
-
+        configuration = plaid.Configuration(
+            host=plaid.Environment.Sandbox,
+            api_key={
+                'clientId': os.getenv('PLAID_CLIENT_ID'),
+                'secret': os.getenv('PLAID_SECRET_KEY'),
+                #'plaidVersion': '2020-09-14'
+            }
+        )
+        
+        api_client = plaid.ApiClient(configuration)
+        client = plaid_api.PlaidApi(api_client)
         #Retrieve user id from request body
         user_id = request.data.get('user_id')
         #Retrieve the user object from the database by id
@@ -1090,17 +1100,16 @@ class PlaidLinkTokenView(APIView):
         client_user_id = user.id
         # Create a link_token for the given user
         request = LinkTokenCreateRequest(
-                products=[Products("auth")],
-                client_name="Plaid Test App",
-                country_codes=[CountryCode('US')],
-                redirect_uri='https://domainname.com/oauth-page.html',
-                language='en',
-                webhook='https://webhook.example.com',
-                user=LinkTokenCreateRequestUser(
-                    # client_user_id=client_user_id
-                    client_user_id="1"
-                )
+            products=[Products("auth")],
+            client_name="Plaid Test App",
+            country_codes=[CountryCode('US')],
+            redirect_uri='https://domainname.com/oauth-page.html',
+            language='en',
+            webhook='https://webhook.example.com',
+            user=LinkTokenCreateRequestUser(
+                client_user_id=client_user_id
             )
+        )
         response = client.link_token_create(request)
         # Send the data to the client
         return Response(response.to_dict(), status=status.HTTP_200_OK)
