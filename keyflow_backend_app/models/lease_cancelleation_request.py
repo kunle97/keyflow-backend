3f3cfab50d050c5fcf68c61cd00990946de2bbba
kunle <adekunle.ashton@gmail.com>
2023-12-01 10:20:28 -0500

    Updated lease cancellation migrations

--- keyflow_backend_app/models/lease_cancelleation_request.py
+++ keyflow_backend_app/models/lease_cancelleation_request.py
@@ -6,19 +6,19 @@ from keyflow_backend_app.models.rental_unit import RentalUnit
 from keyflow_backend_app.models.lease_agreement import LeaseAgreement
     
 class LeaseCancellationRequest(models.Model):
-    tenant = models.ForeignKey(User, on_delete=models.CASCADE, default=None)#tenant that created the lease cancellation request
-    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None) #land lord the lease cancellation request is for
+    tenant = models.ForeignKey(User, on_delete=models.CASCADE, default=None, related_name='cancellation_requests_as_tenant')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, default=None, related_name='cancellation_requests_as_user')
     rental_unit = models.ForeignKey(RentalUnit, on_delete=models.CASCADE)
     lease_agreement = models.ForeignKey(LeaseAgreement, on_delete=models.CASCADE, default=None)
     request_date = models.DateTimeField()
     is_approved = models.BooleanField(default=False)
-    rental_property = models.ForeignKey(RentalProperty, on_delete=models.CASCADE,default=None)
-    comments = models.TextField()
-    created_at = models.DateTimeField(default=datetime.now,  blank=True)
-    updated_at = models.DateTimeField(default=datetime.now,  blank=True)
+    rental_property = models.ForeignKey(RentalProperty, on_delete=models.CASCADE, default=None)
+    comments = models.TextField(default=None, blank=True, null=True)
+    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    updated_at = models.DateTimeField(default=datetime.now, blank=True)
 
     class Meta:
         db_table = 'lease_cancellation_requests'
 
     def __str__(self):
-        return f"Cancellation Request for {self.tenant} on Unit {self.unit}"
+        return f"Cancellation Request for {self.tenant} on Unit {self.rental_unit}"
