49ca9019e9559272add34c389158430099b32801
Adekunle Ademefun <adekunle.ashton@gmail.com>
2025-07-21 20:24:49 -0400

    fixed circular dependancy issue in migrations

--- keyflow_backend_app/views/auth.py
+++ keyflow_backend_app/views/auth.py
@@ -1,3 +1,5 @@
+# users/auth.py
+
 from datetime import timedelta
 import os
 import logging
@@ -5,16 +7,14 @@ from postmarker.core import PostmarkClient
 from dotenv import load_dotenv
 from django.utils import timezone
 from keyflow_backend_app.helpers.owner_plan_access_control import OwnerPlanAccessControl
-from keyflow_backend_app.models.expiring_token import ExpiringToken
+from token_extension.models import ExpiringToken
 from rest_framework.decorators import action
 from rest_framework.response import Response
-from rest_framework import viewsets
-from rest_framework import status
+from rest_framework import viewsets, status
 from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
 from rest_framework.views import APIView
 from django.contrib.auth import login
-from rest_framework.authentication import TokenAuthentication, SessionAuthentication
+from rest_framework.authentication import SessionAuthentication
 from keyflow_backend_app.authentication import ExpiringTokenAuthentication
 from keyflow_backend_app.serializers.account_type_serializer import OwnerSerializer, TenantSerializer
 from ..models.user import User
@@ -25,35 +25,25 @@ from ..serializers.user_serializer import UserSerializer
 from ..serializers.transaction_serializer import TransactionSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import filters
-from rest_framework import status
-from rest_framework.response import Response
-from rest_framework.views import APIView
 
 logger = logging.getLogger(__name__)
 load_dotenv()
 
-# Create the login endpoint view
 class UserLoginView(APIView):
-    def post(self, request): # POST /api/auth/login/
+    def post(self, request):
         email = request.data.get("email")
         password = request.data.get("password")
-        user = User.objects.filter(email=email).first()
         remember_me = request.data.get("remember_me")
 
+        expiration_days = 30 if remember_me else 7
 
-        expiration_time_in_days = 1
-        if user is None:
-            return Response(
-                {"message": "Error logging you in.", "status": status.HTTP_404_NOT_FOUND},
-                status=status.HTTP_404_NOT_FOUND,
-            )
-
-        if not user.check_password(password):
+        user = User.objects.filter(email=email).first()
+        if not user or not user.check_password(password):
             return Response(
                 {"message": "Invalid email or password.", "status": status.HTTP_400_BAD_REQUEST},
                 status=status.HTTP_400_BAD_REQUEST,
             )
-        
+
         if not user.is_active:
             return Response(
                 {"message": "User account is not active. Please check your email for an activation link.", "status": status.HTTP_400_BAD_REQUEST},
@@ -61,93 +51,77 @@ class UserLoginView(APIView):
             )
 
         login(request, user)
+        expiration_date = timezone.now() + timedelta(days=expiration_days)
+        token = self.manage_token(user, expiration_date)
 
-        if remember_me:
-            expiration_time_in_days = 30    
-        else:
-            expiration_time_in_days = 7
-        
-        # Set expiration to 1 minute for demonstration purposes, modify as needed
-        expiration_date = timezone.now() + timedelta(days=expiration_time_in_days)
-        # expiration_date = timezone.now() + timedelta(minutes=1)
+        user_serializer = UserSerializer(user)
 
-        # Check if user is an owner
         if user.account_type == "owner":
             try:
                 owner = Owner.objects.get(user=user)
-                owner_plan_permissions = OwnerPlanAccessControl(owner)
-                plan_data = owner_plan_permissions.plan_data
-                token = self.manage_token(user, expiration_date)
-                user_serializer = UserSerializer(instance=user)
-                owner_serializer = OwnerSerializer(instance=owner)
-                return Response(
-                    {
-                        "message": "User logged in successfully.",
-                        "user": user_serializer.data,
-                        "owner": owner_serializer.data,
-                        "token": token.key,
-                        "token_expiration_date": expiration_date,
-                        "subscription_plan_data": plan_data,
-                        "statusCode": status.HTTP_200_OK,
-                        "owner_id": owner.pk,
-                        "isAuthenticated": True,
-                    },
-                    status=status.HTTP_200_OK,
-                )
+                owner_serializer = OwnerSerializer(owner)
+                plan_data = OwnerPlanAccessControl(owner).plan_data
+
+                return Response({
+                    "message": "User logged in successfully.",
+                    "user": user_serializer.data,
+                    "owner": owner_serializer.data,
+                    "token": token.key,
+                    "token_expiration_date": token.expiring_token.expiration_date,
+                    "subscription_plan_data": plan_data,
+                    "statusCode": status.HTTP_200_OK,
+                    "owner_id": owner.pk,
+                    "isAuthenticated": True,
+                }, status=status.HTTP_200_OK)
+
             except Owner.DoesNotExist:
                 return Response(
                     {"message": "Owner account not found.", "status": status.HTTP_404_NOT_FOUND},
                     status=status.HTTP_404_NOT_FOUND,
                 )
-        
-        # Check if user is a tenant
+
         elif user.account_type == "tenant":
             try:
                 tenant = Tenant.objects.get(user=user)
-                token = self.manage_token(user, expiration_date)
-                user_serializer = UserSerializer(instance=user)
-                tenant_serializer = TenantSerializer(instance=tenant)
-                return Response(
-                    {
-                        "message": "User logged in successfully.",
-                        "user": user_serializer.data,
-                        "tenant": tenant_serializer.data,
-                        "token": token.key,
-                        "token_expiration_date": expiration_date,
-                        "statusCode": status.HTTP_200_OK,
-                        "owner_id":tenant.owner.pk,
-                        "tenant_id": tenant.pk,
-                        "isAuthenticated": True,
-                    },
-                    status=status.HTTP_200_OK,
-                )
+                tenant_serializer = TenantSerializer(tenant)
+
+                return Response({
+                    "message": "User logged in successfully.",
+                    "user": user_serializer.data,
+                    "tenant": tenant_serializer.data,
+                    "token": token.key,
+                    "token_expiration_date": token.expiring_token.expiration_date,
+                    "statusCode": status.HTTP_200_OK,
+                    "owner_id": tenant.owner.pk,
+                    "tenant_id": tenant.pk,
+                    "isAuthenticated": True,
+                }, status=status.HTTP_200_OK)
+
             except Tenant.DoesNotExist:
                 return Response(
                     {"message": "Tenant account not found.", "status": status.HTTP_404_NOT_FOUND},
                     status=status.HTTP_404_NOT_FOUND,
                 )
 
-        else:
-            return Response(
-                {"message": "Invalid account type.", "status": status.HTTP_400_BAD_REQUEST},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-
+        return Response(
+            {"message": "Invalid account type.", "status": status.HTTP_400_BAD_REQUEST},
+            status=status.HTTP_400_BAD_REQUEST,
+        )
 
     def manage_token(self, user, expiration_date):
-        existing_token = ExpiringToken.objects.filter(user=user).first()
-        if existing_token:
-            logger.info(f"Deleting existing token for user {user.id}")
-            existing_token.delete()
+        # Remove old token (if any)
+        Token.objects.filter(user=user).delete()
 
-        token = ExpiringToken.objects.create(
-            user=user,
-            expiration_date=expiration_date,
-            key=Token.generate_key()
+        # Create new token
+        token = Token.objects.create(user=user)
+
+        # Link expiring metadata
+        ExpiringToken.objects.create(
+            token=token,
+            expiration_date=expiration_date
         )
-        logger.info(f"Created new token for user {user.id}")
+
         return token
-    
 
 class UserLogoutView(APIView):
     authentication_classes = [ExpiringTokenAuthentication]
@@ -155,149 +129,99 @@ class UserLogoutView(APIView):
     def post(self, request):
         user = request.user
         if user.is_authenticated:
-            # Check if the user has an expiring token
-            expiring_token = ExpiringToken.objects.filter(user=user).first()
-            if expiring_token:
-                expiring_token.delete()  # Delete the expiring token
-                return Response(
-                    {"message": "User logged out successfully."},
-                    status=status.HTTP_200_OK,
-                )
-            else:
-                return Response(
-                    {"message": "No active token found for the user."},
-                    status=status.HTTP_404_NOT_FOUND,
-                )
-        else:
-            return Response(
-                {"message": "User is not authenticated."},
-                status=status.HTTP_401_UNAUTHORIZED,
-            )
- 
-# create an endpoint to activate the account of a new user that will set the  is_active field to true
+            token = Token.objects.filter(user=user).first()
+            if token:
+                token.delete()
+                return Response({"message": "User logged out successfully."}, status=status.HTTP_200_OK)
+            return Response({"message": "No active token found."}, status=status.HTTP_404_NOT_FOUND)
+        return Response({"message": "User is not authenticated."}, status=status.HTTP_401_UNAUTHORIZED)
+
+
 class UserActivationView(APIView):
     def post(self, request):
-        # Verify that the account activation token is valid
-        account_activation_token = AccountActivationToken.objects.get(
-            token=request.data.get("activation_token")
-        )
-        if account_activation_token is None:
-            return Response(
-                {"message": "Invalid token.", "status": status.HTTP_400_BAD_REQUEST},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+        token_value = request.data.get("activation_token")
+        try:
+            account_activation_token = AccountActivationToken.objects.get(token=token_value)
+        except AccountActivationToken.DoesNotExist:
+            return Response({"message": "Invalid token."}, status=status.HTTP_400_BAD_REQUEST)
 
-        # retrieve user via account activation token
-        user = User.objects.get(email=account_activation_token.email)
+        try:
+            user = User.objects.get(email=account_activation_token.email)
+        except User.DoesNotExist:
+            return Response({"message": "User not found."}, status=status.HTTP_404_NOT_FOUND)
 
-        if user is None:
-            return Response(
-                {"message": "Error activating user."}, status=status.HTTP_404_NOT_FOUND
-            )
         user.is_active = True
         user.save()
-        # Delete the account activation token
         account_activation_token.delete()
-        return Response(
-            {
-                "account_type": user.account_type,
-                "message": "User activated successfully.",
-                "status": status.HTTP_200_OK,
-            },
-            status=status.HTTP_200_OK,
-        )
-   
-#Create a class that has a post method to check if the email exists in the database
+
+        return Response({
+            "account_type": user.account_type,
+            "message": "User activated successfully.",
+            "status": status.HTTP_200_OK,
+        })
+
+
 class UserEmailCheckView(APIView):
     def post(self, request):
         email = request.data.get("email")
-        user = User.objects.filter(email=email).exists()
-        if user:
-            return Response(
-                {"message": "Email already exists.", "status": status.HTTP_400_BAD_REQUEST},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        return Response(
-            {"message": "Email does not exist.", "status": status.HTTP_200_OK},
-            status=status.HTTP_200_OK,
-        )
+        exists = User.objects.filter(email=email).exists()
+        return Response({
+            "message": "Email already exists." if exists else "Email does not exist.",
+            "status": status.HTTP_400_BAD_REQUEST if exists else status.HTTP_200_OK,
+        })
 
 
-#Create a class that has a post method to check if a username exists in the database
 class UsernameCheckView(APIView):
     def post(self, request):
         username = request.data.get("username")
-        user = User.objects.filter(username=username).exists()
-        if user:
-            return Response(
-                {"message": "Username already exists.", "status": status.HTTP_400_BAD_REQUEST},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
-        return Response(
-            {"message": "Username does not exist.", "status": status.HTTP_200_OK},
-            status=status.HTTP_200_OK,
-        )
+        exists = User.objects.filter(username=username).exists()
+        return Response({
+            "message": "Username already exists." if exists else "Username does not exist.",
+            "status": status.HTTP_400_BAD_REQUEST if exists else status.HTTP_200_OK,
+        })
+
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     authentication_classes = [ExpiringTokenAuthentication, SessionAuthentication]
 
-    filter_backends = [
-        DjangoFilterBackend,
-        filters.SearchFilter,
-        filters.OrderingFilter,
-    ]
+    filter_backends = [DjangoFilterBackend, filters.SearchFilter, filters.OrderingFilter]
     filterset_fields = ["account_type", "is_active"]
     search_fields = ["first_name", "last_name", "email"]
     ordering_fields = ["first_name", "last_name", "email", "created_at"]
 
-    # Create a function to change password
     @action(detail=True, methods=["post"], url_path="change-password")
     def change_password(self, request, pk=None):
         user = self.get_object()
-        data = request.data.copy()
-        old_password = data["old_password"]
-        new_password = data["new_password"]
+        old_password = request.data.get("old_password")
+        new_password = request.data.get("new_password")
+
         if user.check_password(old_password):
             user.set_password(new_password)
             user.save()
-            #Create an email notification for the user that the password has been changed
-            postmark   =  PostmarkClient(server_token=os.getenv("POSTMARK_SERVER_TOKEN"))
-            to_email = ""
-            if os.getenv("ENVIRONMENT") == "development":
-                to_email = "keyflowsoftware@gmail.com"
-            else:
-                to_email = user.email
+
+            postmark = PostmarkClient(server_token=os.getenv("POSTMARK_SERVER_TOKEN"))
+            to_email = "keyflowsoftware@gmail.com" if os.getenv("ENVIRONMENT") == "development" else user.email
+
             postmark.emails.send(
                 From=os.getenv("KEYFLOW_SENDER_EMAIL"),
                 To=to_email,
                 Subject="Keyflow Password Changed",
-                HtmlBody=f"Your password has been changed successfully. If you did not make this change, please contact us immediately.",
+                HtmlBody="Your password has been changed successfully. If you did not make this change, please contact us immediately.",
             )
-            
-            return Response(
-                {
-                    "message": "Password changed successfully.",
-                    "status": status.HTTP_200_OK,
-                },
-                status=status.HTTP_200_OK,
-            )
-        return Response(
-            {"message": "Error changing password."}, status=status.HTTP_400_BAD_REQUEST
-        )
 
-    # GET: api/users/{id}/transactions
-    # Create a function to retrieve all transactions for a specific user
+            return Response({"message": "Password changed successfully."}, status=status.HTTP_200_OK)
+
+        return Response({"message": "Error changing password."}, status=status.HTTP_400_BAD_REQUEST)
+
     @action(detail=True, methods=["get"], url_path="transactions")
     def transactions(self, request, pk=None):
         user = self.get_object()
+        if user.id != request.user.id:
+            return Response({"detail": "Unauthorized."}, status=status.HTTP_403_FORBIDDEN)
+
         transactions = Transaction.objects.filter(user=user)
         serializer = TransactionSerializer(transactions, many=True)
-        if user.id == request.user.id:
-            return Response(serializer.data)
-        return Response(
-            {"detail": "You do not have permission to perform this action."},
-            status=status.HTTP_403_FORBIDDEN,
-        )
-
+        return Response(serializer.data)
+    
